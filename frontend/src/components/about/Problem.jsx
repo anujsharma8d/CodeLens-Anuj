49489d847c3137b39ba40984952972d72222331e
Anuj Sharma <anujsharma8d@gmail.com>
2026-06-14 02:07:15 +0530

    Fix typo in Problem.jsx description

--- frontend/src/components/about/Problem.jsx
+++ frontend/src/components/about/Problem.jsx
@@ -27,7 +27,7 @@ const Problem = () => {
                         <svg height="50" width="50" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><title>Codeforces icon</title><path d="M4.5 7.5C5.328 7.5 6 8.172 6 9v10.5c0 .828-.672 1.5-1.5 1.5h-3C.673 21 0 20.328 0 19.5V9c0-.828.673-1.5 1.5-1.5h3zm9-4.5c.828 0 1.5.672 1.5 1.5v15c0 .828-.672 1.5-1.5 1.5h-3c-.827 0-1.5-.672-1.5-1.5v-15c0-.828.673-1.5 1.5-1.5h3zm9 7.5c.828 0 1.5.672 1.5 1.5v7.5c0 .828-.672 1.5-1.5 1.5h-3c-.828 0-1.5-.672-1.5-1.5V12c0-.828.672-1.5 1.5-1.5h3z" /></svg>
                         <div>
                             <h1 className='font-bold text-2xl'>Codeforces</h1>
-                            <p className='max-w-[220px]'>Shows competitive programmingbut not for development skills.</p>
+                            <p className='max-w-[220px]'>Shows competitive programming but not for development skills.</p>
                         </div>
                     </div>
                     <div className='p-3 px-10 flex items-center gap-5'>
